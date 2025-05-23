2817591f2c8e502cc574e9dee8490136ea180aaa
Lucas Almeida Petrolli <lucaslap27@gmail.com>
2025-05-23 11:27:14 -0400

    feat: implementa services de curso, e cria rota de buscar alunos vinculados com um curso

--- routes/routes_cursos.py
+++ routes/routes_cursos.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Body, Request, Response, HTTPException, status, Depends
+from fastapi import APIRouter, Body, Request, Response, HTTPException, status, Depends, Query
 from fastapi.encoders import jsonable_encoder
 from typing import List
 
 from dependencias.curso_dependece import get_curso_service
 from models.cursos import ListModelCursos, UpdateModelCursos
+from models.paginacaoModel import PaginacaoAlunosResponse
 from services.curso_service import CursoService
 
 router = APIRouter()
@@ -13,52 +14,23 @@ COLLECTION_NAME = "cursos"
 def listar_cursos(service: CursoService = Depends(get_curso_service)):
     return service.listar_cursos()
 
+@router.get("/listar-curso-id", response_description="Buscar curso por id", status_code=status.HTTP_200_OK, response_model=ListModelCursos)
+def buscar_curso_id(id: str, service: CursoService = Depends(get_curso_service)):
+    return service.buscar_curso_por_id(id)
+
+@router.get("/listar-alunos-por-curso", response_description="Lista alunos por curso paginado", status_code=status.HTTP_200_OK, response_model=PaginacaoAlunosResponse)
+def listar_alunos_por_curso(id: str, page: int = Query(1, gt=0),
+    limit: int = Query(10, gt=0, le=100), service: CursoService = Depends(get_curso_service)):
+    return service.listar_curso_alunos(id, page, limit)
+
 @router.post("/criar-curso", response_description='Cadastrar curso', status_code=status.HTTP_201_CREATED,response_model=ListModelCursos)
-def create_list(request: Request, list: ListModelCursos = Body(...)):
-    list = jsonable_encoder(list)
-    new_list_item = request.app.database[COLLECTION_NAME].insert_one(list)
-    created_list_item = request.app.database[COLLECTION_NAME].find_one({
-        "_id": new_list_item.inserted_id
-    })
-    return created_list_item
+def create_list(list: ListModelCursos = Body(...), service: CursoService = Depends(get_curso_service)):
+    return service.criar_curso(list)
 
 @router.put("/atualizar-curso", response_description="Atualizar Curso", response_model=ListModelCursos)
-def update_curso(id: str, request: Request, curso_update: UpdateModelCursos = Body(...)):
-    update_data = curso_update.dict(exclude_unset=True)
-
-    if not update_data:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Nenhum dado fornecido para atualização"
-        )
-
-    update_result = request.app.database[COLLECTION_NAME].update_one(
-        {"_id": id},
-        {"$set": update_data}
-    )
-
-    if update_result.modified_count == 0:
-        raise HTTPException(
-            status_code=status.HTTP_304_NOT_MODIFIED,
-            detail=f"Curso com ID {id} não foi modificado"
-        )
-
-    if (
-            updated_curso := request.app.database[COLLECTION_NAME].find_one({"_id": id})
-    ) is not None:
-        return updated_curso
-
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Curso com ID {id} não encontrado"
-    )
+def update_curso(id: str, curso_update: UpdateModelCursos = Body(...), service: CursoService = Depends(get_curso_service)):
+    return service.update_curso(id, curso_update)
 
 @router.delete('/delete-curso', response_description="Deleta Curso")
-def delete_curso(id: str, request: Request, response: Response):
-    delete_result = request.app.database[COLLECTION_NAME].delete_one({"_id": id})
-
-    if delete_result.deleted_count == 1:
-        response.status_code = status.HTTP_204_NO_CONTENT
-        return response
-
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Item with {id} not found")
+def delete_curso(id: str, service: CursoService = Depends(get_curso_service)):
+    return service.deletar_curso(id)
